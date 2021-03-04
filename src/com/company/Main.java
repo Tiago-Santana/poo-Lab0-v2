ad72b4a94426b5f15fcd3698c67495dcc2a5ba6b
Tiago-Santana <200221064@estudantes.ips.pt>
2021-03-04 12:47:25 +0000

    Nível 4 - Atualização do AppStart

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,19 @@
 package com.company;
 
+
+import static com.company.Size.MEDIUM;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
+        //Size beverage = new Size(MEDIUM);
+        Size size = MEDIUM;
+        System.out.println("Name: " + size.getDescription());
+        System.out.println("toString(): " + size.toString());
+        System.out.println("Ordem: " + size.ordinal());
+        System.out.println("Valor minimo: " + size.getMinValue());
+        System.out.println("Valor máximo: " + size.getMaxValue());
+        System.out.println("Código: " + size.getCode());
     }
 }
